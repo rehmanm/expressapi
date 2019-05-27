2296fab335634ea7a89473f8339d94ffd9f92f13
rehmanm <muhammad.rehman@ibexglobal.com>
2019-05-27 16:38:13 +0500

    completed

---
+++ server.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const express = require("express");
+
+const itemsRouter = require("./routes/items");
+
+
+const app=express();
+
+app.use(express.json());
+
+app.use("/items", itemsRouter);
+
+app.use("/", (req, res) =>{
+    res.send("todo api works");
+});
+
+
+
+const server = http.createServer(app);
+
+const port = 3000;
+
+server.listen(port);
+
+console.debug(`server is running on port: ${port}`)
